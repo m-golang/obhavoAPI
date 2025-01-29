91bf847fb92ba81b3fd5c36ebd3c13d2c4285a0b
m-golang <mirjalol.golang@gmail.com>
2025-01-29 09:29:24 +0500

    Create migration files for users table with columns and indexes

---
+++ internal/model/user_model.go
@@ -0,0 +1,6 @@
+package model
+
+func (mysql *Mysql) InsertUser(name, surname, username string, password_hash []byte) error {
+
+	return nil
+}
---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_username ON users (username);
