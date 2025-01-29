c5662304120061d08acfbdfa087d3d029e6ad6ce
m-golang <mirjalol.golang@gmail.com>
2025-01-30 00:12:36 +0500

    Added migration files to store user API Keys:URL params catching like key, q (query);

--- api/helpers/validators.go
+++ api/helpers/validators.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/gin-gonic/gin"
 	validation "github.com/go-ozzo/ozzo-validation"
 )
 
@@ -17,7 +18,7 @@ import (
 // It also ensures that the password and confirm password match.
 func ValidatePassword(password string) error {
 
-	if len(password) == 0 || len(strings.TrimSpace(password))==0{
+	if len(password) == 0 || len(strings.TrimSpace(password)) == 0 {
 		return fmt.Errorf("password cannot be empty or just spaces")
 	}
 
@@ -35,4 +36,19 @@ func ValidatePassword(password string) error {
 	}
 
 	return nil
+}
+
+func GetParametersFromUrl(c *gin.Context) (string, string, error) {
+	apiKey := c.Query("key")
+	if len(apiKey) == 0 || len(strings.TrimSpace(apiKey)) == 0 {
+		return "", "", fmt.Errorf("API key is missing or invalid. Please include a valid API key in your request.")
+	}
+
+	query := c.Query("q")
+	if len(query) == 0 || len(strings.TrimSpace(query)) == 0 {
+
+		return "", "", fmt.Errorf("Parameter q is missing.")
+	}
+
+	return apiKey, query, nil
 }
