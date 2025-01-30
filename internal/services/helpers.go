f55c3195336df7a8f3311706183f8bcd92c73dfd
m-golang <mirjalol.golang@gmail.com>
2025-01-30 12:34:34 +0500

    Add formatWeatherData function to process and format weather data

---
+++ internal/services/helpers.go
@@ -0,0 +1,78 @@
+package services
+
+
+
+func formatWeatherData(weatherData Weather) FormattedWeatherData {
+	var formattedData FormattedWeatherData
+	formattedData.Name = weatherData.Location.Name
+	formattedData.Country = weatherData.Location.Country
+	formattedData.Lat = weatherData.Location.Lat
+	formattedData.Lon = weatherData.Location.Lon
+
+	// Set Temperature Color
+	formattedData.TempC = weatherData.Current.TempC
+	formattedData.TempColor = getTempColor(formattedData.TempC)
+
+	// Set Wind Color
+	formattedData.WindKph = weatherData.Current.WindKph
+	formattedData.WindColor = getWindColor(formattedData.WindKph)
+
+	// Set Cloud Color
+	formattedData.Cloud = weatherData.Current.Cloud
+	formattedData.CloudColor = getCloudColor(formattedData.Cloud)
+
+	return formattedData
+}
+
+
+func getTempColor(tempC float64) string {
+	if tempC < -20 {
+		return "#003366" // Deep Blue
+	} else if tempC >= -20 && tempC < -10 {
+		return "#4A90E2" // Ice Blue
+	} else if tempC >= -10 && tempC < 0 {
+		return "#B3DFFD" // Light Blue
+	} else if tempC >= 0 && tempC < 10 {
+		return "#E6F7FF" // Pale Grayish Blue
+	} else if tempC >= 10 && tempC < 20 {
+		return "#D1F2D3" // Light Green
+	} else if tempC >= 20 && tempC < 30 {
+		return "#FFFACD" // Soft Yellow
+	} else if tempC >= 30 && tempC < 40 {
+		return "#FFCC80" // Light Orange
+	} else if tempC >= 40 && tempC < 50 {
+		return "#FF7043" // Deep Orange
+	} else if tempC >= 50 {
+		return "#D32F2F" // Bright Red
+	}
+	return "#FFFFFF" // Default color if no condition matches
+}
+
+func getWindColor(windKph float64) string {
+	if windKph >= 0 && windKph < 10 {
+		return "#E0F7FA" // Light Cyan
+	} else if windKph >= 10 && windKph < 20 {
+		return "#B2EBF2" // Pale Blue
+	} else if windKph >= 20 && windKph < 40 {
+		return "#4DD0E1" // Soft Teal
+	} else if windKph >= 40 && windKph < 60 {
+		return "#0288D1" // Bright Blue
+	} else if windKph >= 60 {
+		return "#01579B" // Deep Navy Blue
+	}
+	return "#FFFFFF" // Default color if no condition matches
+}
+func getCloudColor(cloud int) string {
+	if cloud >= 0 && cloud < 10 {
+		return "#FFF9C4" // Light Yellow
+	} else if cloud >= 10 && cloud < 30 {
+		return "#FFF176" // Soft Yellow
+	} else if cloud >= 30 && cloud < 60 {
+		return "#E0E0E0" // Light Gray
+	} else if cloud >= 60 && cloud < 90 {
+		return "#9E9E9E" // Gray
+	} else if cloud >= 90 && cloud <= 100 {
+		return "#616161" // Dark Gray
+	}
+	return "#FFFFFF" // Default color if no condition matches
+}
--- internal/services/weatherapi_service.go
+++ internal/services/weatherapi_service.go
@@ -50,58 +50,7 @@ func (s *WeatherAPIService) FetchWeatherData(q string) (*FormattedWeatherData, e
 		return nil, fmt.Errorf("error occured while unmarsheling json: %w", err)
 	}
 
-	var formattedData FormattedWeatherData
-	formattedData.Name = weatherData.Location.Name
-	formattedData.Country = weatherData.Location.Country
-	formattedData.Lat = weatherData.Location.Lat
-	formattedData.Lon = weatherData.Location.Lon
-
-	formattedData.TempC = weatherData.Current.TempC
-	if formattedData.TempC < -20 {
-		formattedData.TempColor = "#003366" // Deep Blue
-	} else if formattedData.TempC >= -20 && formattedData.TempC < -10 {
-		formattedData.TempColor = "#4A90E2" // Ice Blue
-	} else if formattedData.TempC >= -10 && formattedData.TempC < 0 {
-		formattedData.TempColor = "#B3DFFD" // Light Blue
-	} else if formattedData.TempC >= 0 && formattedData.TempC < 10 {
-		formattedData.TempColor = "#E6F7FF" // Pale Grayish Blue
-	} else if formattedData.TempC >= 10 && formattedData.TempC < 20 {
-		formattedData.TempColor = "#D1F2D3" // Light Green
-	} else if formattedData.TempC >= 20 && formattedData.TempC < 30 {
-		formattedData.TempColor = "#FFFACD" // Soft Yellow
-	} else if formattedData.TempC >= 30 && formattedData.TempC < 40 {
-		formattedData.TempColor = "#FFCC80" // Light Orange
-	} else if formattedData.TempC >= 40 && formattedData.TempC < 50 {
-		formattedData.TempColor = "#FF7043" // Deep Orange
-	} else if formattedData.TempC >= 50 {
-		formattedData.TempColor = "#D32F2F" // Bright Red
-	}
-
-	formattedData.WindKph = weatherData.Current.WindKph
-	if formattedData.WindKph >= 0 && formattedData.WindKph < 10 {
-		formattedData.WindColor = "#E0F7FA" // Light Cyan
-	} else if formattedData.WindKph >= 10 && formattedData.WindKph < 20 {
-		formattedData.WindColor = "#B2EBF2" // Pale Blue
-	} else if formattedData.WindKph >= 20 && formattedData.WindKph < 40 {
-		formattedData.WindColor = "#4DD0E1" // Soft Teal
-	} else if formattedData.WindKph >= 40 && formattedData.WindKph < 60 {
-		formattedData.WindColor = "#0288D1" // Bright Blue
-	} else if formattedData.WindKph >= 60 {
-		formattedData.WindColor = "#01579B" // Deep Navy Blue
-	}
-
-	formattedData.Cloud = weatherData.Current.Cloud
-	if formattedData.Cloud >= 0 && formattedData.Cloud < 10 {
-		formattedData.CloudColor = "#FFF9C4" // Light Yellow
-	} else if formattedData.Cloud >= 10 && formattedData.Cloud < 30 {
-		formattedData.CloudColor = "#FFF176" // Soft Yellow
-	} else if formattedData.Cloud >= 30 && formattedData.Cloud < 60 {
-		formattedData.CloudColor = "#E0E0E0" // Light Gray
-	} else if formattedData.Cloud >= 60 && formattedData.Cloud < 90 {
-		formattedData.CloudColor = "#9E9E9E" // Gray
-	} else if formattedData.Cloud >= 90 && formattedData.Cloud <= 100 {
-		formattedData.CloudColor = "#616161" // Dark Gray
-	}
+	formattedData := formatWeatherData(weatherData)
 
 	return &formattedData, nil
 }
